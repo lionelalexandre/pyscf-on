9cb58c717a1b22eea61bbd5d949bb83b125bdc59
Lionel <lionel.truflandier@gmail.com>
2025-01-22 14:43:01 +0100

    add my_simple_hf

--- examples/dm/my_00-simple_hf.py
+++ examples/dm/my_00-simple_hf.py
@@ -13,6 +13,10 @@ import sys
 import pyscf
 from pyscf import gto, scf
 from pyscf import lib
+import imp
+import numpy
+print(imp.find_module('pyscf'))
+
 #log = lib.logger.Logger(sys.stdout, 9)
 
 #mol = pyscf.M(
@@ -58,14 +62,61 @@ from pyscf import gto, scf
 
 mol = gto.Mole()
 mol.atom = open('glycine.xyz').read()
-mol.basis = '6-31g**'
-mol.verbose =20
+mol.basis = 'ccpvtz'
+mol.verbose = 20
 mol.build()
-myhf = mol.HF_DM(max_cycle = 0)
-myhf.kernel()
+#myhf = mol.HF_DM(max_cycle = 0)
+#print('##########',myhf)
+#myhf.kernel()
+mf = scf.RHF(mol).set(conv_tol=1e-10,conv_check=True)
+mf.kernel(dmp_scf=True)
+e_tot_rhf = mf.energy_tot()
 
-import imp
-imp.find_module('pyscf')
+mf = scf.RHF_DM(mol).set(conv_tol=1e-10,conv_check=True)
+mf.kernel(dmp_scf=True)
+e_tot_rhf_dm = mf.energy_tot()
+#myf = scf.hf_dm.SCF(mol).set(conv_tol=1e-8,conv_check=True)
+#myf.max_cycle = 30
+#myf.kernel(dmp_scf=True)
+
+mol.charge = 1
+mol.spin = 3
+mf = scf.UHF(mol).set(conv_tol=1e-10,conv_check=True)
+mf.kernel(dmp_scf=True)
+e_tot_uhf = mf.energy_tot()
+
+mol.charge = 1
+mol.spin = 3
+mf = scf.UHF_DM(mol).set(conv_tol=1e-10,conv_check=True)
+mf.kernel(dmp_scf=True)
+e_tot_uhf_dm = mf.energy_tot()
+
+mol.charge = 1
+mol.spin = 3
+mf = scf.ROHF(mol).set(conv_tol=1e-10,conv_check=True)
+mf.kernel()
+e_tot_rohf = mf.energy_tot()
+
+mol.charge = 1
+mol.spin = 3
+mf = scf.ROHF_DM(mol).set(conv_tol=1e-10,conv_check=True)
+mf.kernel()
+e_tot_rohf_dm = mf.energy_tot()
+
+print('e_tot_rhf    ',e_tot_rhf)
+print('e_tot_rhf_dm ',e_tot_rhf_dm)
+print('e_tot_uhf    ',e_tot_uhf)
+print('e_tot_uhf_dm ',e_tot_uhf_dm)
+print('e_tot_rohf   ',e_tot_rohf)
+print('e_tot_rohf_dm',e_tot_rohf_dm)
+
+#print(myf.conv_tol_grad)
+#print(myf.e_tot)
+#print(myf.mo_energy)
+#print(myf.mo_coeff)
+#print(myf.mo_occ)
+
+#myf._finalize()
 
 #myhf.analyze()
 #mol = gto.M(
