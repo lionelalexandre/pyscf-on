da86b35e737573c0101a312672715ff3ea3a38b9
ClemenceDevos <cledevos@LAPTOP-F4SVUPKH>
2025-02-07 11:28:02 +0100

    benchmark diag + purif

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -24,9 +24,9 @@ for mol_file in molecules:
     mol.build()
 
     # diagonalization
-    #mf = scf.RHF(mol).set(conv_tol=conv_tolerance, conv_check=True)
-    #mf.kernel(dmp_scf=False)
-    #e_tot_rhf = mf.energy_tot()
+    mf = scf.RHF(mol).set(conv_tol=conv_tolerance, conv_check=True)
+    mf.kernel(dmp_scf=False)
+    e_tot_rhf = mf.energy_tot()
 
     # purification
     mf = scf.RHF_DM(mol).set(conv_tol=conv_tolerance, conv_check=True)
@@ -34,7 +34,7 @@ for mol_file in molecules:
     e_tot_rhf_dm = mf.energy_tot()
 
     # Print results
-    #print(f'e_tot_rhf for {mol_file}: {e_tot_rhf:.6f}')
+    print(f'e_tot_rhf for {mol_file}: {e_tot_rhf:.6f}')
     print(f'e_tot_rhf_dm for {mol_file}: {e_tot_rhf_dm:.6f}')
     print(f'Matrix size (NAO): {mol.nao_nr()}')
      
